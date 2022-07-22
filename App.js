ec1d07d8e75aae15b49b6819574d26b361dd17c7
Jacophobia <jacophilia@gmail.com>
2022-07-22 00:07:35 -0600

    Successfully queried from firestore and got some data

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 import { StyleSheet, Text, View } from 'react-native';
 
+import firestore from "@react-native-firebase/firestore";
+
 const App = () => {
   const [greeting, setGreeting] = useState('loading...');
 
+  const getTestData = async () => {
+    const userDocument = await firestore().collection("Test").doc("123").get()
+      .catch(error => console.error(error));
+    console.log(userDocument);
+    setGreeting(userDocument._data.greeting);
+  };
+  getTestData();
   return (
     <View style={styles.enclosingView}>
       <Text style={styles.helloWorldText}>Hello World!</Text>
