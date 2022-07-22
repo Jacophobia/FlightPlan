b89a9d6286015a2ef0cdecef4aee2332054ef3c6
Jacophobia <jacophilia@gmail.com>
2022-07-21 22:43:34 -0600

    Added some configuration to the gradle files for android

---
+++ App.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import { StyleSheet, Text, View } from 'react-native';
+
+const App = () => {
+  const [greeting, setGreeting] = useState('');
+
+  return (
+    <View style={styles.enclosingView}>
+      <Text style={styles.helloWorldText}>Hello World!</Text>
+      <Text style={styles.helloWorldText}>{greeting}</Text>
+    </View>
+  );
+};
+
+export default App;
+
+const styles = new StyleSheet.create({
+  enclosingView: {
+    height: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#348CCB',
+  },
+  helloWorldText: {
+    color: '#CB7334',
+  },
+});
