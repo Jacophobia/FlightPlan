a4ed4440edf83e97c9b830ddbc690a7cb40b00ac
Jacob Morgan <i35623@verisk.com>
2022-08-20 22:46:50 -0600

    Added a dropdown and navigation imports

--- Pages/PageComponents/FlightTrackDropDown.js
+++ Pages/PageComponents/FlightTrackDropDown.js
@@ -1,11 +1,40 @@
 import React, { useState } from "react";
-import { StyleSheet, Text, View, Image } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Image, Pressable } from 'react-native';
 import { testFirestoreConnection } from "./Firebase/Firestore";
 import { FlightTrackInput } from "./PageComponents/FlightTrackInput";
 import LinearGradient from 'react-native-linear-gradient';
+import { Button, Menu, Divider, Provider } from 'react-native-paper';
 
 export function FlightTrackDropDown(props) {
-  return (<Text>Flight Track Drop Down Not Yet Initialized</Text>);
+  const [visible, setVisible] = React.useState(true);
+
+  const openMenu = () => setVisible(true);
+
+  const closeMenu = () => setVisible(false);
+
+  return (
+    <ScrollView style={{width: '100%', borderWidth: 1, borderColor: 'red', }}>
+      <View
+        style={{
+          paddingTop: 50,
+          flexDirection: 'row',
+          justifyContent: 'center',
+          width: '100%',
+        }}>
+        <Menu
+          style={{width: '100%', borderWidth: 1, borderColor: 'red',}}
+          visible={visible}
+          onDismiss={closeMenu}
+          anchor={<Pressable style={{ width: '100%'}} onPress={openMenu}><Text>Show menu</Text></Pressable>}>
+          <Menu.Item onPress={() => {setVisible(false)}} title="Item 1" />
+          <Divider />
+          <Menu.Item onPress={() => {setVisible(false)}} title="Item 2" />
+          <Divider />
+          <Menu.Item onPress={() => {setVisible(false)}} title="Item 3" />
+        </Menu>
+      </View>
+    </ScrollView>
+  );
 }
 
 const styles = StyleSheet.create({
--- index.js
+++ index.js
@@ -6,12 +6,15 @@ import { AppRegistry } from 'react-native';
 import App from './App';
 import { name as appName } from './app.json';
 import { Provider as PaperProvider } from 'react-native-paper';
+import { NavigationContainer } from "@react-navigation/native";
 
 export default function Main() {
   return (
-    <PaperProvider>
-      <App />
-    </PaperProvider>
+    <NavigationContainer>
+      <PaperProvider>
+        <App />
+      </PaperProvider>
+    </NavigationContainer>
   );
 }
 
